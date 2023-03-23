32be46d08d2256a88b7e4b1db81a913b614de6e1
Neru-K <atokoam@gmail.com>
2023-03-24 01:38:44 +0900

    submitted

--- tessoku-book/src/bin/a02.rs
+++ tessoku-book/src/bin/a02.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,x:i32,
+        a :[i32;n],
+    }
+
+    let mut is_exist = "No";
+
+    for i in 0..a.len() {
+        if a[i] == x {
+            is_exist = "Yes";
+        }
+    }
+
+    println!("{}", is_exist);
 }
