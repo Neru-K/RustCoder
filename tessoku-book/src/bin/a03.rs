410ddb935c05d27e82cb4edea43146ecb48de935
Neru-K <atokoam@gmail.com>
2023-03-28 23:10:16 +0900

    submitted

--- tessoku-book/src/bin/a03.rs
+++ tessoku-book/src/bin/a03.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,k:usize,
+        p:[usize;n],
+        q:[usize;n],
+    }
+
+    let mut result = "No";
+
+    'outer: for i in 0..p.len() {
+        for j in 0..q.len() {
+            if p[i] + q[j] == k {
+                result = "Yes";
+                break 'outer;
+            }
+        }
+    }
+
+    println!("{}", result);
 }
