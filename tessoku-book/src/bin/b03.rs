5efebaf5a075ecff01ea7ec83df6ea3cbd278ea7
Neru-K <atokoam@gmail.com>
2023-03-28 23:26:53 +0900

    submitted

--- tessoku-book/src/bin/b03.rs
+++ tessoku-book/src/bin/b03.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a:[usize;n],
+    }
+    let mut result = "No";
+
+    'outer: for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                continue;
+            }
+            for k in 0..n {
+                if i == k || j == k {
+                    continue;
+                }
+
+                if a[i] + a[j] + a[k] == 1000 {
+                    result = "Yes";
+                    break 'outer;
+                }
+            }
+        }
+    }
+
+    println!("{}", result);
 }
