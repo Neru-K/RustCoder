e0e5232ae64eaea0f17cce52cecb2a62d208ac92
Neru-K <atokoam@gmail.com>
2023-03-29 23:50:00 +0900

    submitted

--- abs/src/bin/abc081b.rs
+++ abs/src/bin/abc081b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n:usize,
+        mut a: [usize; n],
+    }
+    let mut has_even = true;
+    let mut count = 0;
+
+    while has_even == true {
+        for i in 0..n {
+            if a[i] % 2 != 0 {
+                has_even = false;
+            } else {
+                a[i] = a[i] / 2;
+            }
+        }
+        if has_even == true {
+            count = count + 1;
+        }
+    }
+
+    println!("{}", count);
 }
