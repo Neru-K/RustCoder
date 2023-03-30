e115c164c16fabca188a675034cdfda6e0d3bf02
Neru-K <atokoam@gmail.com>
2023-03-30 22:06:36 +0900

    submitted

--- abs/src/bin/abc087b.rs
+++ abs/src/bin/abc087b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: usize,
+        b: usize,
+        c: usize,
+        x: usize,
+    }
+
+    let mut count = 0;
+
+    for i in 0..=a {
+        for j in 0..=b {
+            for k in 0..=c {
+                let sum = 500 * i + 100 * j + 50 * k;
+                if sum == x {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", count);
 }
