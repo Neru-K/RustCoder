376c5f926359a6be7863ed09221793fa68509e3e
Neru-K <atokoam@gmail.com>
2023-04-02 22:47:51 +0900

    submitted

--- tessoku-book/src/bin/a04.rs
+++ tessoku-book/src/bin/a04.rs
@@ -1,3 +1,13 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+    }
+
+    for x in (0..10).rev() {
+        let wari = 2usize.pow(x);
+        print!("{}", (n / wari) % 2);
+    }
+    println!();
 }
--- tessoku-book/src/bin/b04.rs
+++ tessoku-book/src/bin/b04.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        mut n: String,
+    }
+
+    let mut result = 0;
+    let mut x = 1;
+
+    for c in n.chars().rev() {
+        if c == '1' {
+            result += x;
+        }
+        x = x * 2;
+    }
+
+    println!("{}", result);
 }
