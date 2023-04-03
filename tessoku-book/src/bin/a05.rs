c67865428ee6e91cf37df1d93b0dbde6890dc7e1
Neru-K <atokoam@gmail.com>
2023-04-03 23:29:04 +0900

    submitted

--- tessoku-book/src/bin/a05.rs
+++ tessoku-book/src/bin/a05.rs
@@ -1,3 +1,24 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,k:usize,
+    }
+
+    let mut result: usize = 0;
+
+    'outer: for x in 1..=n {
+        for y in 1..=n {
+            if x + y > k {
+                continue 'outer;
+            }
+            let z = k - x - y;
+
+            if z >= 1 && z <= n {
+                result += 1;
+            }
+        }
+    }
+
+    println!("{}", result);
 }
