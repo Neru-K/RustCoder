64ed8fa25eb1d20f38c008976ef86003e6f7f635
Neru-K <atokoam@gmail.com>
2023-04-04 23:45:20 +0900

    submitted

--- abs/src/bin/abc088b.rs
+++ abs/src/bin/abc088b.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut num:[usize;n],
+    }
+
+    num.sort();
+    num.reverse();
+    let mut bob = 0;
+    let mut alice = 0;
+
+    for i in 0..num.len() {
+        if i % 2 == 0 {
+            alice += num[i];
+        } else {
+            bob += num[i];
+        }
+    }
+    println!("{}", alice - bob);
 }
