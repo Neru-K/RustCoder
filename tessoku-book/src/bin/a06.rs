b44eef58ee91c7ea395e5df877c90b2ce8701a54
Neru-K <atokoam@gmail.com>
2023-04-05 23:46:40 +0900

    submitted

--- tessoku-book/src/bin/a06.rs
+++ tessoku-book/src/bin/a06.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize, q: usize,
+        a: [usize;n],
+        lr: [[usize;2];q],
+    }
+
+    let mut sum = vec![0];
+
+    for i in 1..=n {
+        sum.push(sum[i - 1] + a[i - 1]);
+    }
+
+    for j in 0..q {
+        println!("{}", sum[lr[j][1]] - sum[lr[j][0] - 1]);
+    }
 }
