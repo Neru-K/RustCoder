20611414c944da5b740a5c738e5c2bd4df2812a1
Neru-K <atokoam@gmail.com>
2023-04-06 23:57:46 +0900

    submitted

--- tessoku-book/src/bin/b06.rs
+++ tessoku-book/src/bin/b06.rs
@@ -1,3 +1,30 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a:[usize;n],
+        q:usize,
+        lr:[[usize;2];q],
+    }
+
+    let mut sum = vec![0];
+
+    for i in 1..=n {
+        sum.push(sum[i - 1] + a[i - 1]);
+    }
+    //println!("{:?}", sum);
+
+    for j in 0..q {
+        let all = lr[j][1] as f32 - lr[j][0] as f32 + 1.0;
+        let outcome = sum[lr[j][1]] as f32 - sum[lr[j][0] - 1] as f32;
+
+        if all / 2.0 == outcome {
+            println!("draw");
+        } else if all / 2.0 < outcome {
+            println!("win");
+        } else if all / 2.0 > outcome {
+            println!("lose");
+        }
+    }
 }
