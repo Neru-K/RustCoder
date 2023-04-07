81b3ee61c95cdd6378e536c4af4abaee5888b3e5
Neru-K <atokoam@gmail.com>
2023-04-07 23:53:13 +0900

    submitted

--- abs/src/bin/abc085b.rs
+++ abs/src/bin/abc085b.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+use std::collections::HashSet;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut d:[usize;n],
+    }
+
+    d.sort();
+    let set: HashSet<_> = d.drain(..).collect();
+    d.extend(set.into_iter());
+    println!("{}", d.len());
 }
--- tessoku-book/src/bin/a07.rs
+++ tessoku-book/src/bin/a07.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        d: usize,
+        n:usize,
+        lr:[[usize;2];n],
+    }
+
+    let mut b: Vec<isize> = vec![0; d];
+    //let mut result: Vec<usize> = vec![0; d];
+
+    println!("{:?}", b);
+
+    for i in 0..n {
+        println!("lr[i][0]{}", lr[i][0]);
+        b[lr[i][0] - 1] += 1;
+        println!("lr[i][0]{}", lr[i][1]);
+        b[lr[i][1] - 1] -= 1;
+
+        println!("{:?}", b);
+    }
+
+    //result[0] = b[0];
+
+    for j in 1..=d {
+        println!("{}", b[j - 1] + b[j]);
+    }
 }
