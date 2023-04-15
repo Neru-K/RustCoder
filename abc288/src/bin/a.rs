16e045834d59782ec451c5c51abc49ca933f9c45
Neru-K <atokoam@gmail.com>
2023-04-15 23:03:34 +0900

    submitted

---
+++ abc288/src/bin/a.rs
@@ -0,0 +1,12 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        n:usize,
+        ab: [[isize;2];n],
+    }
+
+    for i in 0..n {
+        println!("{}", ab[i][0] + ab[i][1]);
+    }
+}
