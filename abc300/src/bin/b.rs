2187ac74b747319275bb1efabebe47721ee5dd57
Neru-K <atokoam@gmail.com>
2023-04-29 22:43:27 +0900

    created directories

---
+++ abc300/src/bin/b.rs
@@ -0,0 +1,13 @@
+use proconio::input;
+
+fn main() {
+    input! {
+        h:usize,w:usize,
+        mut a:[[chars;w];h],
+        mut b:[[chars;w];h],
+    }
+
+    let mut result = "No";
+
+    println!("{}", result);
+}
